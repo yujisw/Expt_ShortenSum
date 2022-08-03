4bb0c9fae4a4b848f009ccd8622869a85de47161
yujisw <yuji.1277@gmail.com>
2022-08-04 03:55:44 +0900

    make variant of desired length

--- data_utils/make_oracle_length_data.py
+++ data_utils/make_oracle_length_data.py
@@ -4,3 +4,12 @@ data = [s[:-1] for s in data]
 tgt_length_list = [len(s.split(" ")) for s in data]
 with open("data/desired_lengths/test.oracle", "w") as f:
     f.writelines(["{}\n".format(length) for length in tgt_length_list])
+
+with open("data/desired_lengths/test.oracle+20", "w") as f:
+    f.writelines(["{}\n".format(length+20) for length in tgt_length_list])
+with open("data/desired_lengths/test.oracle+10", "w") as f:
+    f.writelines(["{}\n".format(length+10) for length in tgt_length_list])
+with open("data/desired_lengths/test.oracle-10", "w") as f:
+    f.writelines(["{}\n".format(length-10) for length in tgt_length_list])
+with open("data/desired_lengths/test.oracle-20", "w") as f:
+    f.writelines(["{}\n".format(length-20) for length in tgt_length_list])
